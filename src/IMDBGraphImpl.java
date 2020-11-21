2c5588ca02f5764592c4b211342de214d195fd25
Alec Miller <ahmiller@wpi.edu>
2020-11-21 17:28:30 -0500

    javadoc and a few more comments for IMDBGraphImpl

--- src/IMDBGraphImpl.java
+++ src/IMDBGraphImpl.java
@@ -7,7 +7,12 @@ public class IMDBGraphImpl implements IMDBGraph {
 	private static List<ActorNode> actorList;
 	private static List<MovieNode> movieList;
 
-	// Implement me
+	/**
+	 * creates an IMDBGraphImpl instance that loads and parses the actors and actresses files
+	 * @param actorsFilename the name of the file containing the information about actors
+	 * @param actressesFilename the name of the file containing the information about actresses
+	 * @throws IOException if the filenames are inaccessible
+	 */
 	public IMDBGraphImpl (String actorsFilename, String actressesFilename) throws IOException {
 		actorList = new ArrayList<>();
 		movieList = new ArrayList<>();
@@ -15,17 +20,27 @@ public class IMDBGraphImpl implements IMDBGraph {
 		processActors(actressesFilename);
 	}
 
-	// Implement me
+	/**
+	 * returns the arraylist of ActorNode stored in actorList
+	 * @return actorList
+	 */
 	public Collection<? extends Node> getActors () {
 		return actorList;
 	}
 
-	// Implement me
+	/**
+	 * returns the arraylist of MovieNode stored in movieList
+	 * @return movieList
+	 */
 	public Collection<? extends Node> getMovies () {
 		return movieList;
 	}
 
-	// Implement me
+	/**
+	 * returns a MovieNode whose title is the param name
+	 * @param name the title of a movie to find
+	 * @return movie a MovieNode with the same title as param name
+	 */
 	public Node getMovie (String name) {
 		for(MovieNode movie: movieList){
 			if(name.equals(movie.getName())){
@@ -35,7 +50,11 @@ public class IMDBGraphImpl implements IMDBGraph {
 		return null;
 	}
 
-	// Implement me
+	/**
+	 * returns an ActorNode whose name is the param name
+	 * @param name the name of an actor to find
+	 * @return actor an ActorNode with same name as param name
+	 */
 	public Node getActor (String name) {
 		for(ActorNode actor: actorList){
 			if(name.equals(actor.getName())){
@@ -60,7 +79,7 @@ public class IMDBGraphImpl implements IMDBGraph {
 	 * Scans an IMDB file for its actors/actresses and movies
 	 * @param filename the movie file to parse
 	 */
-	protected static void processActors (String filename /* add other parameters as desired */) throws IOException {
+	protected static void processActors (String filename) throws IOException {
 		final Scanner s = new Scanner(new File(filename), "ISO-8859-1");
 
 		// Skip until:  Name...Titles
@@ -81,7 +100,7 @@ public class IMDBGraphImpl implements IMDBGraph {
 				int idxOfTab = line.indexOf("\t");
 				if (idxOfTab > 0) {  // not at beginning of line => new actor
 					if(counter == 0){
-						actorList.remove(actorList.size()-1);
+						actorList.remove(actorList.size()-1); // removes the actor from the list if they are not in any movies
 					}
 					counter = 0;
 					String actorName = line.substring(0, idxOfTab);
@@ -94,7 +113,7 @@ public class IMDBGraphImpl implements IMDBGraph {
 					int lastIdxOfTab = line.lastIndexOf("\t");
 					final String movieName = parseMovieName(line.substring(lastIdxOfTab + 1));
 					boolean found = false;
-					for(MovieNode movie: movieList){
+					for(MovieNode movie: movieList){ // if the movie is already in movieList
 						if(movie.getName().equals(movieName)){
 							movie.addActor(mostRecent);
 							actorList.get(actorList.size()-1).addMovie(movie);
@@ -103,7 +122,7 @@ public class IMDBGraphImpl implements IMDBGraph {
 						}
 					}
 
-					if(!found){
+					if(!found){ // if the movie is not already in movieList
 						MovieNode newMovie = new MovieNode(movieName);
 						newMovie.addActor(mostRecent);
 						movieList.add(newMovie);
